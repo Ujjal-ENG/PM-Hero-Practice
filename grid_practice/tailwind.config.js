2fd58ebbbd61ade32c2a30eb8e58515663c9b489
user <ujjalroy7862@gmail.com>
2023-01-23 19:57:08 +0600

    learn the git layout

---
+++ grid_practice/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  mode: 'jit',
+  purge: [],
+  darkMode: "media",
+  plugins: [],
+};
