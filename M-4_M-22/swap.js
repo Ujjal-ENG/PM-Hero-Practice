a64dff6101f9049e4ea838e50c5947166d8a5264
user <ujjalroy7862@gmail.com>
2023-02-02 19:58:46 +0600

    first apporact for swaping

--- M-4_M-22/swap.js
+++ M-4_M-22/swap.js
@@ -2,10 +2,12 @@ let first = 5;
 let second = 7;
 let swap;
 
+// 1st approach for swaping
 swap = first;
-
 first = second;
-
 second = swap;
+
+// 2nd apporach for swaping
+[first, second] = [second, first];
 
 console.log(first, second);
