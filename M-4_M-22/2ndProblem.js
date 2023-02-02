25ed2928ca7d0b27b12cb35f7e7386fced011028
user <ujjalroy7862@gmail.com>
2023-02-02 20:14:15 +0600

    solved the first problem

---
+++ M-4_M-22/2ndProblem.js
@@ -0,0 +1,13 @@
+let num1 = 84
+let num2 = 99
+let num3 = 77
+
+if (num1 > num2 && num1 > num3) {
+  console.log("Jim")
+} else if (num2 > num1 && num2 > num3) {
+  console.log("Dela");
+  
+} else {
+  console.log("Chinku");
+  
+}
