ee1e9130c7199f6fab59225bd897ec32947f31f3
user <ujjalroy7862@gmail.com>
2023-02-02 21:11:56 +0600

    done the this problem

--- M-4_M-22/reverseString.js
+++ M-4_M-22/reverseString.js
@@ -1,12 +1,17 @@
-function reverStr(text) {
+// function reverStr(text) {
   
-  let arr = []
-  for (let i = text.length-1; i >= 0; i--){
-    arr.push(text[i])
-  }
-  return arr
+//   let arr = []
+//   for (let i = text.length-1; i >= 0; i--){
+//     arr.push(text[i])
+//   }
+//   return arr
+// }
+
+// const res = reverStr("helllo");
+
+// console.log(res);
+
+function add(a, b){
+  return a + b;
 }
-
-const res = reverStr("helllo");
-
-console.log(res);
+console.log(add("adam" + "eve"))
