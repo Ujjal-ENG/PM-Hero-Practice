0eb07894643ce90b3ac8f864b9e8df39ce358139
user <ujjalroy7862@gmail.com>
2023-02-04 00:36:27 +0600

    learn the nested fuction calling

--- M-4_M-23/script.js
+++ M-4_M-23/script.js
@@ -1,3 +1,17 @@
-const calcAge2 = (birthyear) => 2023 - birthyear;
+// const calcAge2 = (birthyear) => 2023 - birthyear;
 
-console.log(calcAge2(1998));
+// console.log(calcAge2(1998));
+
+function cutFruitPices(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePices = cutFruitPices(apples);
+  const orangePices = cutFruitPices(oranges);
+
+  const juice = `Juice with ${applePices} apples and ${orangePices}`;
+  return juice;
+}
+
+console.log(fruitProcessor(2, 3));
