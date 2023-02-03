ba2983ca1b4cfcf6f4bf15cc0c3cd915a6ecf381
user <ujjalroy7862@gmail.com>
2023-02-04 02:06:43 +0600

    done this problem

--- M-4_M-23/script.js
+++ M-4_M-23/script.js
@@ -16,22 +16,39 @@ function cutFruitPices(fruit) {
 
 // console.log(fruitProcessor(2, 3));
 
-const calcAverage = (num1, num2, num3) => {
-  return Math.round((num1 + num2 + num3) / 3);
-};
+// const calcAverage = (num1, num2, num3) => {
+//   return Math.round((num1 + num2 + num3) / 3);
+// };
 
-const checkWinner = (avgDolphins, avgKolas) => {
-  if (avgDolphins >= 2 * avgKolas) {
-    return `Dolphins is win the trophy🏆 and the score is ${avgDolphins} vs ${avgKolas}`;
-  } else if (avgKolas >= 2 * avgDolphins) {
-    return `Kolas is win the trophy🏆 and the score is ${avgDolphins} vs ${avgKolas}`;
-  } else {
-    return "No teams win this match";
+// const checkWinner = (avgDolphins, avgKolas) => {
+//   if (avgDolphins >= 2 * avgKolas) {
+//     return `Dolphins is win the trophy🏆 and the score is ${avgDolphins} vs ${avgKolas}`;
+//   } else if (avgKolas >= 2 * avgDolphins) {
+//     return `Kolas is win the trophy🏆 and the score is ${avgDolphins} vs ${avgKolas}`;
+//   } else {
+//     return "No teams win this match";
+//   }
+// };
+
+// let avgDolphins = calcAverage(44, 23, 71);
+// let avgKolas = calcAverage(65, 54, 49);
+// console.log(avgDolphins, avgKolas);
+// const res = checkWinner(avgDolphins, avgKolas);
+// console.log(res);
+// const name = "Ujjal";
+
+// console.log((name[0] = "s"));
+// console.log(name);
+
+const calcTip = (tip) => {
+  if (tip >= 50 && tip <= 300) {
+    return Math.round(tip * (15 / 100));
   }
+  return Math.round(tip * (20 / 100));
 };
 
-let avgDolphins = calcAverage(44, 23, 71);
-let avgKolas = calcAverage(65, 54, 49);
-console.log(avgDolphins, avgKolas);
-const res = checkWinner(avgDolphins, avgKolas);
-console.log(res);
+const bills = [125, 555, 40];
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+console.log(bills, tips);
