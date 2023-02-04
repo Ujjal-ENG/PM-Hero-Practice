b20c3575f5f1aeefdb01a6dea2902d40813da454
user <ujjalroy7862@gmail.com>
2023-02-04 13:13:40 +0600

    track the position

--- M-4_M-23/script.js
+++ M-4_M-23/script.js
@@ -124,4 +124,6 @@ let dice = Math.trunc(Math.random() * 6) + 1;
 while (dice !== 6) {
   console.log(dice);
   dice = Math.trunc(Math.random() * 6) + 1;
+
+  if (dice === 6) console.log("Loop is about to end");
 }
