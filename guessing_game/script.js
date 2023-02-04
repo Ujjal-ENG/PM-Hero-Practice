9ba8449c3582e1038903a4d1e8c61c9cffbc0954
user <ujjalroy7862@gmail.com>
2023-02-04 16:05:43 +0600

    learn the DOM

---
+++ guessing_game/script.js
@@ -0,0 +1,10 @@
+document.querySelector(".message").textContent = "🏆🏆Correct Number!!";
+
+document.querySelector(".number").textContent = 12;
+
+document.querySelector(".score").textContent = 30;
+
+console.log(document.querySelector(".guess").value);
+
+document.querySelector(".guess").value = 12;
+
