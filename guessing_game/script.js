6e19b456131e769ee3a6c23a689e836be2cf6b18
user <ujjalroy7862@gmail.com>
2023-02-04 17:23:53 +0600

    manipulate color from the body

--- guessing_game/script.js
+++ guessing_game/script.js
@@ -6,19 +6,29 @@ document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", () => {
   const guessValue = Number(document.querySelector(".guess").value);
+
+  // when player given not any value
   if (!guessValue) {
     document.querySelector(".message").textContent =
       "😒 No Number are given...";
+
+    // When Player Wins
   } else if (guessValue === secretNumber) {
     document.querySelector(".message").textContent =
       "🏆🏆Hurray!!You won the game...";
-  } else if (guessValue > secretNumber) {
 
+    document.querySelector("body").style.backgroundColor = "#60b347";
+
+    document.querySelector(".number").style.width = "30rem";
+
+    // when the player guess high number from given number
+  } else if (guessValue > secretNumber) {
     if (score > 0) {
       document.querySelector(".message").textContent = "Too High!!☝️";
       score--;
       document.querySelector(".score").textContent = score;
-      
+
+      // when the player guess low number from given number
     } else {
       document.querySelector(".message").textContent =
         "Your are Score is not enough to play the game!!";
