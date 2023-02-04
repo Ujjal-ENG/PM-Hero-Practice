4ae849b2c7e9db99b9c92242ad242792885609da
user <ujjalroy7862@gmail.com>
2023-02-04 17:10:50 +0600

    create the some functionality

--- guessing_game/script.js
+++ guessing_game/script.js
@@ -1,10 +1,36 @@
-document.querySelector(".message").textContent = "🏆🏆Correct Number!!";
+const secretNumber = Math.trunc(Math.random() * 21);
 
-document.querySelector(".number").textContent = 12;
+let score = 20;
 
-document.querySelector(".score").textContent = 30;
+document.querySelector(".number").textContent = secretNumber;
 
-console.log(document.querySelector(".guess").value);
+document.querySelector(".check").addEventListener("click", () => {
+  const guessValue = Number(document.querySelector(".guess").value);
+  if (!guessValue) {
+    document.querySelector(".message").textContent =
+      "😒 No Number are given...";
+  } else if (guessValue === secretNumber) {
+    document.querySelector(".message").textContent =
+      "🏆🏆Hurray!!You won the game...";
+  } else if (guessValue > secretNumber) {
 
-document.querySelector(".guess").value = 12;
-
+    if (score > 0) {
+      document.querySelector(".message").textContent = "Too High!!☝️";
+      score--;
+      document.querySelector(".score").textContent = score;
+      
+    } else {
+      document.querySelector(".message").textContent =
+        "Your are Score is not enough to play the game!!";
+    }
+  } else if (guessValue < secretNumber) {
+    if (score > 0) {
+      document.querySelector(".message").textContent = "Too Low!!👇";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent =
+        "Your are Score is not enough to play the game!!";
+    }
+  }
+});
