aae825105860b01ff2b6af8e71adc8fbdcd61962
user <ujjalroy7862@gmail.com>
2023-02-06 02:03:32 +0600

    done the hoisting

--- practice-js/hoisting.js
+++ practice-js/hoisting.js
@@ -1,3 +1,15 @@
 console.log(a);
 
 var a = "Bangladesh";
+
+var LAN = "JAVA";
+var lan = "JS";
+
+function getLan() {
+  if (!lan) {
+    lan = LAN;
+  }
+  return lan;
+}
+
+console.log(getLan());
