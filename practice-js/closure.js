2daa571f3af791e6594f5c97df9d5841810b0b9f
user <ujjalroy7862@gmail.com>
2023-02-05 21:38:07 +0600

    another example of curring

--- practice-js/closure.js
+++ practice-js/closure.js
@@ -1,12 +1,23 @@
-var num1 = 2;
+// var num1 = 2;
 
-var sum = function () {
-  var num2 = 3;
+// var sum = function () {
+//   var num2 = 3;
+//   return function () {
+//     return num1 + num2;
+//   };
+// };
+
+// var myFunc = sum();
+
+// console.dir(myFunc);
+
+function bankAccount(initialBalance) {
+  var balance = initialBalance;
+
   return function () {
-    return num1 + num2;
+    return balance;
   };
-};
+}
 
-var myFunc = sum();
-
-console.dir(myFunc);
+var account = bankAccount(199);
+console.log(account);
