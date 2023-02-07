f0590c18703d004aaca89cba0439a7d237464fab
user <ujjalroy7862@gmail.com>
2023-02-07 16:39:27 +0600

    done the this keyword example

--- practice-js/thisKeyword.js
+++ practice-js/thisKeyword.js
@@ -1,1 +1,35 @@
-console.log("Ujjal");
+// var printPlayerName = function (name) {
+//   console.log(name);
+// };
+
+// printPlayerName("Ujjal");
+
+// var tamim = {
+//   name: "tamim",
+//   age: 34,
+//   printPlayerName: function () {
+//     console.log(this.name);
+//   },
+// };
+// tamim.printPlayerName();
+
+var Person = function (name, age) {
+  return {
+    name: name,
+    age: age,
+    printName: function () {
+      console.log(this.name);
+    },
+    father: {
+      name: "Mr. XXX",
+      printName: function () {
+        console.log(this.name);
+      },
+    },
+  };
+};
+
+var tamim = Person("Tamim", 35);
+
+tamim.printName();
+tamim.father.printName();
