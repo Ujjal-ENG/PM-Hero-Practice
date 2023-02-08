f618ba6ec3595385709070f01cbfa2dab2b96680
user <ujjalroy7862@gmail.com>
2023-02-09 01:11:52 +0600

    write the function body

--- practice-js/promise.js
+++ practice-js/promise.js
@@ -41,8 +41,33 @@
 // promise1.then((res1) => console.log(res1));
 // promise2.then((res2) => console.log(res2));
 
-function friendlyFunction() {
-  return `hello`;
-}
 
-console.log(friendlyFunction());
+const hasMeeting = false;
+const meeting = new Promise((resolve, reject) => {
+  // do stuff
+  console.log(!hasMeeting);
+  if (!hasMeeting) {
+    const meetingDetails = {
+      name: "Technical Meeting",
+      location: "Google Meet",
+      time: "10:00 PM",
+    };
+    resolve(meetingDetails);
+  } else {
+    reject(new Error("meeting already scheduled"));
+  }
+});
+
+const addToCalender = (meetingDetails) => {
+  const calender = `${meetingDetails.name} has been scheduled on ${meetingDetails.location} at ${meetingDetails.time}`;
+  return Promise.resolve(calender);
+};
+
+meeting
+  .then(addToCalender)
+  .then((res) => {
+    console.log(JSON.stringify(res));
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
