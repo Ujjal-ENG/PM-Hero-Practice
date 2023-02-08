6d1a0162c21fa04e6ecd32047c2031b9dc45962b
user <ujjalroy7862@gmail.com>
2023-02-08 15:16:41 +0600

    map is the higherorder function

--- practice-js/higerOrder.js
+++ practice-js/higerOrder.js
@@ -1,9 +1,15 @@
 // higher order function
 
-function hello() {
-  return () => {
-    console.log("higer order function");
-  };
-}
+// function hello() {
+//   return () => {
+//     console.log("higer order function");
+//   };
+// }
 
-hello()();
+// hello()();
+
+var numbers = [1, 2, 3];
+
+var res = numbers.map((num) => num ** 2);
+
+console.log(res);
