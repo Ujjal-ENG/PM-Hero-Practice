f3a762e44436086bced64ecf06c0b919b4da8d7d
user <ujjalroy7862@gmail.com>
2023-02-09 01:08:05 +0600

    promise for async

--- practice-js/promise.js
+++ practice-js/promise.js
@@ -1,31 +1,42 @@
-const hasMeeting = false;
-const meeting = new Promise((resolve, reject) => {
-  // do stuff
-  console.log(!hasMeeting);
-  if (!hasMeeting) {
-    const meetingDetails = {
-      name: "Technical Meeting",
-      location: "Google Meet",
-      time: "10:00 PM",
-    };
-    resolve(meetingDetails);
-  } else {
-    reject(new Error("meeting already scheduled"));
-  }
+// const hasMeeting = false;
+// const meeting = new Promise((resolve, reject) => {
+//   // do stuff
+//   console.log(!hasMeeting);
+//   if (!hasMeeting) {
+//     const meetingDetails = {
+//       name: "Technical Meeting",
+//       location: "Google Meet",
+//       time: "10:00 PM",
+//     };
+//     resolve(meetingDetails);
+//   } else {
+//     reject(new Error("meeting already scheduled"));
+//   }
+// });
+
+// const addToCalender = (meetingDetails) => {
+//   const calender = `${meetingDetails.name} has been scheduled on ${meetingDetails.location} at ${meetingDetails.time}`;
+//   return Promise.resolve(calender);
+// };
+
+// meeting
+//   .then(addToCalender)
+//   .then((res) => {
+//     console.log(JSON.stringify(res));
+//   })
+//   .catch((err) => {
+//     console.log(err.message);
+//   });
+
+// console.log("Hello");
+
+const promise1 = Promise.resolve(`Promise 1 resolved`);
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(`Promise 2 resolved`);
+  }, 2000);
 });
 
-const addToCalender = (meetingDetails) => {
-  const calender = `${meetingDetails.name} has been scheduled on ${meetingDetails.location} at ${meetingDetails.time}`;
-  return Promise.resolve(calender);
-};
-
-meeting
-  .then(addToCalender)
-  .then((res) => {
-    console.log(JSON.stringify(res));
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
-
-console.log("Hello");
+promise1.then((res1) => console.log(res1));
+promise2.then((res2) => console.log(res2));
