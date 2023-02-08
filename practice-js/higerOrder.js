b0b81ae55a14a27ef528200b4fcc015107c611e9
user <ujjalroy7862@gmail.com>
2023-02-08 15:22:22 +0600

    filter is one of them to the higher order function

--- practice-js/higerOrder.js
+++ practice-js/higerOrder.js
@@ -8,8 +8,31 @@
 
 // hello()();
 
-var numbers = [1, 2, 3];
+// var numbers = [1, 2, 3];
 
-var res = numbers.map((num) => num ** 2);
+// var res = numbers.map((num) => num ** 2);
 
-console.log(res);
+// console.log(res);
+
+var players = [
+  {
+    name: "Sakib",
+    avg: 38.23,
+  },
+  {
+    name: "Tamim",
+    avg: 36.74,
+  },
+  {
+    name: "Musfiq",
+    avg: 36.78,
+  },
+  {
+    name: "Mahmudullah",
+    avg: 37.12,
+  },
+];
+
+var newPlayer = players.filter((player) => player.avg < 37);
+
+console.log(newPlayer);
