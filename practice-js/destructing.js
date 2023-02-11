5ca317612aa11b984bb673c12734b6ee6c5b58f9
user <ujjalroy7862@gmail.com>
2023-02-11 20:59:34 +0600

    change some portion

---
+++ practice-js/destructing.js
@@ -0,0 +1,11 @@
+const resturant = {
+  name: "Classico Italiano",
+  location: "Via Angela",
+  categories: ["Italian", "Pizzeria", "Vegetraian", "Organic"],
+  startMenu: ["Focaccia", "Bruschesta", "Garlic Bread", "Capress Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+};
+
+const [arr] = resturant.categories;
+
+console.log(arr);
