bab2c82de2c46c3394495f2b2b4dd36c8dd6e815
user <ujjalroy7862@gmail.com>
2023-02-12 17:48:20 +0600

    now its fully functional

--- baperBank_Tailwind&JS/deposit.js
+++ baperBank_Tailwind&JS/deposit.js
@@ -8,7 +8,15 @@ depositBTN.addEventListener("click", () => {
   const depositAmount = document.getElementById("depositAmount");
 
   const deop = Number(depositAmount.innerText);
+
+  const balanceToal = document.getElementById("balanceTotal");
+
+  const balanceTotal = Number(balanceToal.innerText);
+
   const depositTotal = String(depoIn + deop);
+  const balanceTotals = String(deop + balanceTotal);
+
+  balanceToal.innerText = balanceTotals;
   depositAmount.innerText = depositTotal;
   depositINput.value = "";
 });
