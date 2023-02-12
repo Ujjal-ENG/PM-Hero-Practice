83eaca271788ae3399627fe68d99678f8f519f41
user <ujjalroy7862@gmail.com>
2023-02-12 17:42:30 +0600

    fixed some bugs

--- baperBank_Tailwind&JS/deposit.js
+++ baperBank_Tailwind&JS/deposit.js
@@ -1,12 +1,14 @@
 const depositBTN = document.getElementById("btnDeposit");
 
 depositBTN.addEventListener("click", () => {
-  const depositINput = Number(document.getElementById("depositInput").value);
+  const depositINput = document.getElementById("depositInput");
+
+  const depoIn = Number(depositINput.value);
 
   const depositAmount = document.getElementById("depositAmount");
 
   const deop = Number(depositAmount.innerText);
-  const depositTotal = String(depositINput + deop);
+  const depositTotal = String(depoIn + deop);
   depositAmount.innerText = depositTotal;
-  console.log(depositINput, deop);
+  depositINput.value = "";
 });
