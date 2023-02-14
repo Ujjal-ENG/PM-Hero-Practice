6bbd63feb9cea83080590480a8362a75621a9a30
user <ujjalroy7862@gmail.com>
2023-02-15 03:12:20 +0600

    another functionality done

--- pin-matcher/index.js
+++ pin-matcher/index.js
@@ -23,3 +23,20 @@ getID("generate-pin").addEventListener("click", () => {
   const getValue = getID("display-pin");
   getValue.value = getPin();
 });
+
+getID("calculator").addEventListener("click", (e) => {
+  const number = e.target.innerText;
+  const previTypeNUmber = getID("typed-numbers").value;
+  if (isNaN(number)) {
+    if (number === "C") {
+      getID("typed-numbers").value = "";
+    } else if (number === "<") {
+      const digits = previTypeNUmber.split("");
+      digits.pop();
+      getID("typed-numbers").value = digits.join("");
+    }
+  } else {
+    const currentTypeNumber = previTypeNUmber + number;
+    getID("typed-numbers").value = currentTypeNumber;
+  }
+});
