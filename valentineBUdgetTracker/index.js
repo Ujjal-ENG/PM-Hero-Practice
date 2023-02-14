7a84538f76ea72848877a99442c2e86aae5641f5
user <ujjalroy7862@gmail.com>
2023-02-14 22:29:37 +0600

    function work well

--- valentineBUdgetTracker/index.js
+++ valentineBUdgetTracker/index.js
@@ -1,12 +1,23 @@
-function getBTN(id) {
-  const btn = document.getElementById(id);
-  return btn;
+function getID(id) {
+  const ID = document.getElementById(id);
+  return ID;
+}
+
+function getTotal(qnID, pID, pIDS) {
+  const qunatityIn = getID(qnID);
+  const qunatityInValue = Number(qunatityIn.value);
+  const cholatePrice = getID(pID).innerText;
+  const totalChPrice = getID(pIDS);
+  const totalPrice = eval(qunatityInValue * cholatePrice);
+  console.log(totalPrice);
+  totalChPrice.innerText = totalPrice;
+  const totalget = getID("toPrice").innerText;
 }
 
 const budgetInput = document.getElementById("success");
 document.getElementById("success-msg").style.display = "none";
 
-getBTN("budgetBTN").addEventListener("click", () => {
+getID("budgetBTN").addEventListener("click", () => {
   const budgetValue = budgetInput.value;
   const parentEl = document.getElementById("parent-input-div");
   const newEl = document.createElement("h1");
@@ -14,4 +25,16 @@ getBTN("budgetBTN").addEventListener("click", () => {
   parentEl.appendChild(newEl);
   document.getElementById("success-msg").style.display = "block";
   budgetInput.value = "";
+});
+
+getID("addtocartBTN1").addEventListener("click", () => {
+  getTotal("quantity1", "choclatePrice", "chPrice");
+});
+
+getID("addtocartBTN2").addEventListener("click", () => {
+  getTotal("quantity2", "rosePrice", "roPrice");
+});
+
+getID("addtocartBTN3").addEventListener("click", () => {
+  getTotal("quantity3", "diaryPrice", "daPrice");
 });
