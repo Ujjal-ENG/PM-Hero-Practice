8a0f9c80c0d78f66c284a0550881586800bd801c
user <ujjalroy7862@gmail.com>
2023-02-15 02:55:04 +0600

    done the generate pin funation

---
+++ pin-matcher/index.js
@@ -0,0 +1,15 @@
+function generatePin() {
+  const random = Math.round(Math.random() * 10000);
+  return random;
+}
+
+function getPin() {
+  const pin = generatePin();
+  const pinString = pin + "";
+  if (pinString.length === 4) {
+    return pin;
+  } else {
+    console.log("Pin not 3 digit found"+pin);
+    return getPin();
+  }
+}
