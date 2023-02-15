487cf8520f0d3f50140c2809492525bf58138c82
user <ujjalroy7862@gmail.com>
2023-02-15 20:52:44 +0600

    done this project very easily

--- 100sJSDOMProject/crud-dom-app/index.js
+++ 100sJSDOMProject/crud-dom-app/index.js
@@ -11,6 +11,13 @@ getId("#addBook").addEventListener("click", (e) => {
   e.preventDefault();
   if (title.value === "" && author.value === "" && year.value === "") {
     alert("Please fill up the all input field");
+  } else {
+    const newRow = document.createElement("tr");
+    newRow.innerHTML = `
+    <th>${title.value}</th>
+    <th>${author.value}</th>
+    <th>${year.value}</th>
+    `;
+    getId("#bookList").appendChild(newRow);
   }
-
 });
