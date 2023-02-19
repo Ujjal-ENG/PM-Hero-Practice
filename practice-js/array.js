3382928a7b8c84b1fd0a5c573070527a49b997a5
user <ujjalroy7862@gmail.com>
2023-02-20 00:08:37 +0600

    start to learn the date object

--- practice-js/array.js
+++ practice-js/array.js
@@ -1,26 +1,29 @@
-const fruits = ["Banana", "Apple", "Orange", "Mango", "Lichi"];
+// const fruits = ["Banana", "Apple", "Orange", "Mango", "Lichi"];
 
-fruits.forEach((el) => console.log(el));
-console.log("finished");
-fruits.splice(1, 0, "Lemon");
+// fruits.forEach((el) => console.log(el));
+// console.log("finished");
+// fruits.splice(1, 0, "Lemon");
 
-console.log(fruits);
-fruits.splice(1, 1);
+// console.log(fruits);
+// fruits.splice(1, 1);
 
-const numbers = [1, 2, 3, 4, 5];
+// const numbers = [1, 2, 3, 4, 5];
 
-// const newNumbers = numbers.reduce((pV, cV) => {
-//   console.log(pV, cV);
-//   return pV + cV;
-// });
-// console.log(newNumbers);
+// // const newNumbers = numbers.reduce((pV, cV) => {
+// //   console.log(pV, cV);
+// //   return pV + cV;
+// // });
+// // console.log(newNumbers);
 
-const funcAvg = (arrs) => {
-  let sum = 0;
-  for (const i of arrs) {
-    sum += Math.pow(i, 2);
-  }
-  return sum / arrs.length;
-};
+// const funcAvg = (arrs) => {
+//   let sum = 0;
+//   for (const i of arrs) {
+//     sum += Math.pow(i, 2);
+//   }
+//   return sum / arrs.length;
+// };
 
-console.log(funcAvg(numbers));
+// console.log(funcAvg(numbers));
+
+
+// Now start to learn the date
