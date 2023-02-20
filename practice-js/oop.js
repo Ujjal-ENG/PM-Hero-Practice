48b497e7a541a43092dfd0c48c5cc2a1e168f06c
user <ujjalroy7862@gmail.com>
2023-02-21 02:20:41 +0600

    learn the inheritances

--- practice-js/oop.js
+++ practice-js/oop.js
@@ -10,8 +10,21 @@ class Vehical {
   }
 }
 
-const car1 = new Vehical("BMW", "CAR", 2012);
-const bike1 = new Vehical("SNAKE-BLADE", "BIKE", 2023);
+class ManufacureCompany extends Vehical {
+  constructor(name, type, year, manfacBy) {
+    super(name, type, year);
+    this.manfacBy = manfacBy;
+  }
+
+  display() {
+    console.log(
+      `${this.name} is the type of ${this.type} vehical which is lunched by ${this.year} and its Manufacture company is ${this.manfacBy}`
+    );
+  }
+}
+
+const car1 = new ManufacureCompany("BMW", "CAR", 2012, "Germany-BMW");
+const bike1 = new ManufacureCompany("SNAKE-BLADE", "BIKE", 2023, "China");
 
 car1.display();
 bike1.display();
