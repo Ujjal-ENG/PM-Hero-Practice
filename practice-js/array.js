13ac4c702fc8aa3bb2de511a6a8aaedeb6d376a6
user <ujjalroy7862@gmail.com>
2023-02-21 00:47:20 +0600

    done the final problem for valu finding

--- practice-js/array.js
+++ practice-js/array.js
@@ -218,15 +218,45 @@ const products = [
 // }, 0);
 // console.log(newAge);
 
-let data = {
-  location: [
-    {
-      latitude: "34.4",
-      longitude: "54.23",
-      city: "Heydrabad",
-      country: "India",
+// let data = {
+//   location: [
+//     {
+//       latitude: "34.4",
+//       longitude: "54.23",
+//       city: "Heydrabad",
+//       country: "India",
+//     },
+//   ],
+// };
+
+// console.log(data.location[0].city);
+
+const user = {
+  id: 1,
+  name: "Leanne Graham",
+  username: "Bret",
+  email: "Sincere@april.biz",
+  address: {
+    street: "Kulas Light",
+    suite: "Apt. 556",
+    city: "Gwenborough",
+    zipcode: "92998-3874",
+    geo: {
+      lat: "-37.3159",
+      lng: "81.1496",
     },
-  ],
+  },
+  phone: "1-770-736-8031 x56442",
+  website: "hildegard.org",
+  company: {
+    name: "Romaguera-Crona",
+    catchPhrase: "Multi-layered client-server neural-net",
+    bs: "harness real-time e-markets",
+  },
 };
 
-console.log(data.location[0].city);
+console.log(user.email);
+console.log(user.address.street);
+console.log(user.address.zipcode);
+console.log(user.address.geo.lng);
+console.log(user.company.catchPhrase);
