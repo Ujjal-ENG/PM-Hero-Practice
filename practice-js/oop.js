4186e850db6b54a32afff903693ae4b7500bd4bb
user <ujjalroy7862@gmail.com>
2023-02-21 02:27:30 +0600

    learn the completly new part iin the js section

--- practice-js/oop.js
+++ practice-js/oop.js
@@ -4,9 +4,7 @@ class Vehical {
   }
 
   display() {
-    console.log(
-      `${this.name} is the type of ${this.type} vehical which is lunched by ${this.year}`
-    );
+    return `${this.name} is the type of ${this.type} vehical which is lunched by ${this.year}`;
   }
 }
 
@@ -16,9 +14,9 @@ class ManufacureCompany extends Vehical {
     this.manfacBy = manfacBy;
   }
 
-  display() {
+  show() {
     console.log(
-      `${this.name} is the type of ${this.type} vehical which is lunched by ${this.year} and its Manufacture company is ${this.manfacBy}`
+      this.display() + "and its Manufacture company is " + this.manfacBy
     );
   }
 }
@@ -26,5 +24,5 @@ class ManufacureCompany extends Vehical {
 const car1 = new ManufacureCompany("BMW", "CAR", 2012, "Germany-BMW");
 const bike1 = new ManufacureCompany("SNAKE-BLADE", "BIKE", 2023, "China");
 
-car1.display();
-bike1.display();
+car1.show();
+bike1.show();
