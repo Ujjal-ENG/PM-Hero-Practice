0c0e9e6e6e7f2117eaa14dc31d5405b0bc68eb90
user <ujjalroy7862@gmail.com>
2023-02-21 22:29:58 +0600

    3rd problem done

--- practice-js/destructing.js
+++ practice-js/destructing.js
@@ -29,34 +29,60 @@
 
 // console.log(data.Sophia.study[1].secondary[1].location, school_name);
 
-let data = [
-  {
-    pHeroCourses: {
-      "course-x": "web development",
-    },
-  },
-  {
-    pHeroCourses: {
-      "course-y": "phitron",
-    },
-  },
-  {
-    pHeroCourses: {
-      "course-z": "acc",
-    },
-  },
-  {
-    pHeroCourses: {
-      "course-xyz": "level-2",
-    },
-    locationField: {
-      "en-US": {
-        lat: 19.28563,
-        lon: 72.8691,
-      },
-    },
-  },
-];
+// let data = [
+//   {
+//     pHeroCourses: {
+//       "course-x": "web development",
+//     },
+//   },
+//   {
+//     pHeroCourses: {
+//       "course-y": "phitron",
+//     },
+//   },
+//   {
+//     pHeroCourses: {
+//       "course-z": "acc",
+//     },
+//   },
+//   {
+//     pHeroCourses: {
+//       "course-xyz": "level-2",
+//     },
+//     locationField: {
+//       "en-US": {
+//         lat: 19.28563,
+//         lon: 72.8691,
+//       },
+//     },
+//   },
+// ];
 
-console.log(data[3].pHeroCourses["course-xyz"]);
-console.log(data[3].locationField["en-US"]["lon"]);
+// console.log(data[3].pHeroCourses["course-xyz"]);
+// console.log(data[3].locationField["en-US"]["lon"]);
+
+let activities = {
+  activity1: [
+    {
+      name: "listen on spotify",
+
+      song_list: {
+        song_1: "abc",
+        song_2: "bcd",
+      },
+      id: 1,
+    },
+    {
+      name: "listen music through bot",
+
+      song_list: {
+        song_1: "wxy",
+        song_2: "xyz",
+      },
+      id: 2,
+    },
+  ],
+};
+
+console.log(activities.activity1[0]["song_list"]["song_1"]);
+console.log(activities.activity1[1]["song_list"]["song_2"]);
