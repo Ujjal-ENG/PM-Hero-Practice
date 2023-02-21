c44bb6e0f0b42372c4fdd33c58acdd651e169410
user <ujjalroy7862@gmail.com>
2023-02-21 22:43:37 +0600

    done the all problem solution from programming hero

--- practice-js/destructing.js
+++ practice-js/destructing.js
@@ -138,34 +138,59 @@
 // console.log(instructor.additionalData.moreDetails["hometown"].state);
 // console.log(instructor.additionalData.moreDetails["countriesLivedIn"][0]);
 
-const studentData = [
-  {
-    class: 10,
-    details: [
-      {
-        studentId: "1",
-        gradingDetails: [{ grade: "A" }],
-      },
-      {
-        studentId: "2",
-        gradingDetails: [{ grade: "B" }],
-      },
-    ],
-  },
-  {
-    class: 11,
-    details: [
-      {
-        studentId: 3,
-        gradingDetails: [{ grade: "B" }],
-      },
-      {
-        studentId: 4,
-        gradingDetails: [{ grade: "D" }],
-      },
-    ],
-  },
-];
+// const studentData = [
+//   {
+//     class: 10,
+//     details: [
+//       {
+//         studentId: "1",
+//         gradingDetails: [{ grade: "A" }],
+//       },
+//       {
+//         studentId: "2",
+//         gradingDetails: [{ grade: "B" }],
+//       },
+//     ],
+//   },
+//   {
+//     class: 11,
+//     details: [
+//       {
+//         studentId: 3,
+//         gradingDetails: [{ grade: "B" }],
+//       },
+//       {
+//         studentId: 4,
+//         gradingDetails: [{ grade: "D" }],
+//       },
+//     ],
+//   },
+// ];
 
-console.log(studentData[1].details[0].gradingDetails[0].grade);
-console.log(studentData[1].details[1].gradingDetails[0].grade);
+// console.log(studentData[1].details[0].gradingDetails[0].grade);
+// console.log(studentData[1].details[1].gradingDetails[0].grade);
+
+let data = {
+  data: [
+    {
+      bookId: 1,
+      bookDetails: {
+        name: "habluder adda",
+        category: "XYZ",
+        price: "20$",
+      },
+      bookCategory: "Basic",
+    },
+    {
+      bookId: 2,
+      bookDetails: {
+        name: "gobluder adda",
+        category: "ABC",
+        price: "40$",
+      },
+      bookCategory: "Beginner",
+    },
+  ],
+};
+console.log(data.data[0].bookDetails.name);
+console.log(data.data[1].bookCategory);
