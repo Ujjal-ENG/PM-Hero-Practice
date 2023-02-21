aa15c3fe8861f9908daec3239cfa1af1dc03b23a
user <ujjalroy7862@gmail.com>
2023-02-21 22:27:25 +0600

    done the 2nd problem

--- practice-js/destructing.js
+++ practice-js/destructing.js
@@ -6,25 +6,57 @@
 //   mainMenu: ["Pizza", "Pasta", "Risotto"],
 // };
 
-let data = {
-  Sophia: {
-    id: 33,
-    study: [
-      {
-        primary: [
-          { school_name: "ABC primary school" },
-          { location: "Peters burg" },
-        ],
-      },
-      {
-        secondary: [
-          { school_name: "ABC secondary school" },
+// let data = {
+//   Sophia: {
+//     id: 33,
+//     study: [
+//       {
+//         primary: [
+//           { school_name: "ABC primary school" },
+//           { location: "Peters burg" },
+//         ],
+//       },
+//       {
+//         secondary: [
+//           { school_name: "ABC secondary school" },
 
-          { location: "St Lorence" },
-        ],
-      },
-    ],
+//           { location: "St Lorence" },
+//         ],
+//       },
+//     ],
+//   },
+// };
+
+// console.log(data.Sophia.study[1].secondary[1].location, school_name);
+
+let data = [
+  {
+    pHeroCourses: {
+      "course-x": "web development",
+    },
   },
-};
+  {
+    pHeroCourses: {
+      "course-y": "phitron",
+    },
+  },
+  {
+    pHeroCourses: {
+      "course-z": "acc",
+    },
+  },
+  {
+    pHeroCourses: {
+      "course-xyz": "level-2",
+    },
+    locationField: {
+      "en-US": {
+        lat: 19.28563,
+        lon: 72.8691,
+      },
+    },
+  },
+];
 
-console.log(data.Sophia.study[1].secondary[1].location, school_name);
+console.log(data[3].pHeroCourses["course-xyz"]);
+console.log(data[3].locationField["en-US"]["lon"]);
