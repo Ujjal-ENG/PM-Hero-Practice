148eee6dbf6c4c24973700a0335778560bda7ccf
user <ujjalroy7862@gmail.com>
2023-02-21 22:40:41 +0600

    done the 6th problem

--- practice-js/destructing.js
+++ practice-js/destructing.js
@@ -115,25 +115,57 @@
 // console.log(students[2222].address["city"]);
 // console.log(students[3333].name);
 
-let instructor = {
-  name: "Jhankar Mahbub",
-  entrepreneur: true,
-  additionalData: {
-    writer: true,
-    favoriteHobbies: ["travelling", "Coding"],
-    books: ["programming er bolod to bos", "programming er coddogosti"],
-    moreDetails: {
-      favoriteBasketballTeam: "XYZ",
-      isYoungest: true,
-      hometown: {
-        city: "ABC",
-        state: "VW",
-      },
-      countriesLivedIn: ["Bangladesh", "New York"],
-    },
-  },
-};
+// let instructor = {
+//   name: "Jhankar Mahbub",
+//   entrepreneur: true,
+//   additionalData: {
+//     writer: true,
+//     favoriteHobbies: ["travelling", "Coding"],
+//     books: ["programming er bolod to bos", "programming er coddogosti"],
+//     moreDetails: {
+//       favoriteBasketballTeam: "XYZ",
+//       isYoungest: true,
+//       hometown: {
+//         city: "ABC",
+//         state: "VW",
+//       },
+//       countriesLivedIn: ["Bangladesh", "New York"],
+//     },
+//   },
+// };
 
-console.log(instructor.additionalData.books[1]);
-console.log(instructor.additionalData.moreDetails["hometown"].state);
-console.log(instructor.additionalData.moreDetails["countriesLivedIn"][0]);
+// console.log(instructor.additionalData.books[1]);
+// console.log(instructor.additionalData.moreDetails["hometown"].state);
+// console.log(instructor.additionalData.moreDetails["countriesLivedIn"][0]);
+
+const studentData = [
+  {
+    class: 10,
+    details: [
+      {
+        studentId: "1",
+        gradingDetails: [{ grade: "A" }],
+      },
+      {
+        studentId: "2",
+        gradingDetails: [{ grade: "B" }],
+      },
+    ],
+  },
+  {
+    class: 11,
+    details: [
+      {
+        studentId: 3,
+        gradingDetails: [{ grade: "B" }],
+      },
+      {
+        studentId: 4,
+        gradingDetails: [{ grade: "D" }],
+      },
+    ],
+  },
+];
+
+console.log(studentData[1].details[0].gradingDetails[0].grade);
+console.log(studentData[1].details[1].gradingDetails[0].grade);
