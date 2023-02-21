f86df14ff082989aff3ef7c4ef02d969e697c960
user <ujjalroy7862@gmail.com>
2023-02-21 22:37:06 +0600

    done the 5th problem

--- practice-js/destructing.js
+++ practice-js/destructing.js
@@ -87,30 +87,53 @@
 // console.log(activities.activity1[0]["song_list"]["song_1"]);
 // console.log(activities.activity1[1]["song_list"]["song_2"]);
 
-let students = {
-  2222: {
-    name: "Jack",
-    section: "C",
-    class: "IX",
-    address: {
-      "building no": 12,
-      street: "St. Jonson",
-      city: "Petersburg",
-      country: "UK",
-    },
-  },
-  3333: {
-    name: "Herry",
-    section: "D",
-    class: "X",
-    address: {
-      "building no": 85,
-      street: "DC road",
-      city: "Kachukhet",
-      country: "Bangladesh",
+// let students = {
+//   2222: {
+//     name: "Jack",
+//     section: "C",
+//     class: "IX",
+//     address: {
+//       "building no": 12,
+//       street: "St. Jonson",
+//       city: "Petersburg",
+//       country: "UK",
+//     },
+//   },
+//   3333: {
+//     name: "Herry",
+//     section: "D",
+//     class: "X",
+//     address: {
+//       "building no": 85,
+//       street: "DC road",
+//       city: "Kachukhet",
+//       country: "Bangladesh",
+//     },
+//   },
+// };
+
+// console.log(students[2222].address["city"]);
+// console.log(students[3333].name);
+
+let instructor = {
+  name: "Jhankar Mahbub",
+  entrepreneur: true,
+  additionalData: {
+    writer: true,
+    favoriteHobbies: ["travelling", "Coding"],
+    books: ["programming er bolod to bos", "programming er coddogosti"],
+    moreDetails: {
+      favoriteBasketballTeam: "XYZ",
+      isYoungest: true,
+      hometown: {
+        city: "ABC",
+        state: "VW",
+      },
+      countriesLivedIn: ["Bangladesh", "New York"],
     },
   },
 };
 
-console.log(students[2222].address["city"]);
-console.log(students[3333].name);
+console.log(instructor.additionalData.books[1]);
+console.log(instructor.additionalData.moreDetails["hometown"].state);
+console.log(instructor.additionalData.moreDetails["countriesLivedIn"][0]);
