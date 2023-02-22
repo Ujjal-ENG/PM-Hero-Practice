5a967f02a00b719e0059273c777d512680b5d2af
user <ujjalroy7862@gmail.com>
2023-02-22 17:10:42 +0600

    initital structure is ready

--- practice-js/CheckValidityAPI.js
+++ practice-js/CheckValidityAPI.js
@@ -1,19 +1,5 @@
-document.getElementById("checkValidity").addEventListener("click", () => {
-  const inputOB = document.getElementById("id1");
+const display = document.getElementById("display");
 
-  if (!inputOB.checkValidity()) {
-    document.getElementById("demo").innerHTML = inputOB.validationMessage;
-  } else {
-    document.getElementById("demo").innerHTML =
-      "great!!! you provide a valid number";
-  }
-});
-const inputOB = document.getElementById("id1");
-inputOB.addEventListener("keyup", () => {
-  if (!inputOB.checkValidity()) {
-    document.getElementById("demo").innerHTML = inputOB.validationMessage;
-  } else {
-    document.getElementById("demo").innerHTML =
-      "great!!! you provide a valid number";
-  }
+document.getElementById("fetchData").addEventListener("click", () => {
+  console.log("ujja Kupa Mama");
 });
