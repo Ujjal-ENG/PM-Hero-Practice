80f9f5e442022be19ecd7b1436255f692d37e51f
user <ujjalroy7862@gmail.com>
2023-02-22 17:16:10 +0600

    fetching data from remore servver

--- practice-js/CheckValidityAPI.js
+++ practice-js/CheckValidityAPI.js
@@ -1,5 +1,9 @@
 const display = document.getElementById("display");
 
 document.getElementById("fetchData").addEventListener("click", () => {
-  console.log("ujja Kupa Mama");
+  fetch("http://127.0.0.1:5500/practice-js/data.txt")
+    .then((res) => res.text())
+    .then((data) => {
+      display.innerText = data;
+    });
 });
