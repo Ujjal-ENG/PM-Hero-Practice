cd8d8fe9ee96a430721fa1188598df79ebed1f56
user <ujjalroy7862@gmail.com>
2023-02-22 23:25:22 +0600

    created my own version API calling

--- AdvanceJS/fetchJS.js
+++ AdvanceJS/fetchJS.js
@@ -1,3 +1,5 @@
-fetch("https://jsonplaceholder.typicode.com/todos/1")
-  .then((response) => response.json())
-  .then((json) => console.log(json.title));
+const url = "https://jsonplaceholder.typicode.com/todos/77";
+
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => console.log(data));
