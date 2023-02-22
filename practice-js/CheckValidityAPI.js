cec84cad697696d282fb882b55ca237a7ab18a9f
user <ujjalroy7862@gmail.com>
2023-02-22 17:18:32 +0600

    data fetching done using async and await function

--- practice-js/CheckValidityAPI.js
+++ practice-js/CheckValidityAPI.js
@@ -1,9 +1,13 @@
 const display = document.getElementById("display");
 
-document.getElementById("fetchData").addEventListener("click", () => {
-  fetch("http://127.0.0.1:5500/practice-js/data.txt")
-    .then((res) => res.text())
-    .then((data) => {
-      display.innerText = data;
-    });
+document.getElementById("fetchData").addEventListener("click", async () => {
+  // fetch("http://127.0.0.1:5500/practice-js/data.txt")
+  //   .then((res) => res.text())
+  //   .then((data) => {
+  //     display.innerText = data;
+  //   });
+
+  const res = await fetch("http://127.0.0.1:5500/practice-js/data.txt");
+  const data = await res.text();
+  display.innerText = data;
 });
