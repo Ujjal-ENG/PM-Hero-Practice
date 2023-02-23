bc25be038228d14453a82e5ebb6dbf18bd69f7eb
user <ujjalroy7862@gmail.com>
2023-02-23 22:42:27 +0600

    done this project

---
+++ AdvanceJS/quotes.js
@@ -0,0 +1,10 @@
+async function quotesCalling() {
+  const data = await fetch("https://api.kanye.rest");
+  const json = await data.json();
+  const showQueto = document.getElementById("quote");
+  showQueto.innerHTML = json.quote;
+}
+
+setInterval(() => {
+  quotesCalling();
+}, 1000);
