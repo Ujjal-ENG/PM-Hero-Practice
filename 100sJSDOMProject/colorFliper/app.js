4370b16a26a1608b8140098427a326201c31e7ef
user <ujjalroy7862@gmail.com>
2023-02-23 19:48:41 +0600

    done the random color for rgb

--- 100sJSDOMProject/colorFliper/app.js
+++ 100sJSDOMProject/colorFliper/app.js
@@ -1,1 +1,17 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", (e) => {
+  document.body.style.backgroundColor = randomColor();
+  color.innerHTML = randomColor();
+});
+
+function randomInt(max, min) {
+  return Math.floor(Math.random() * (max - min + 1) + 0);
+}
+
+function randomColor() {
+  return `rgb(${randomInt(255, 0)},${randomInt(255, 0)},${randomInt(255, 0)})`;
+}
