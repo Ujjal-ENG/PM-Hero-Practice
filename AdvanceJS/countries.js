5ee61ffddea0903ba782715dc76776033c7014a2
user <ujjalroy7862@gmail.com>
2023-02-24 00:33:18 +0600

    done the full project

--- AdvanceJS/countries.js
+++ AdvanceJS/countries.js
@@ -34,6 +34,22 @@ const countriesDetails = async (code) => {
 };
 
 const countryDetail = (data) => {
-  const container = document.getElementById("modal-body")
+  const container = document.getElementById("modal-body");
+  const newDiv = document.createElement("div");
 
+  newDiv.innerHTML = `
+  <h3>Region: ${data.region}</h3>
+  <h3>Sub-Region: ${data.subregion}</h3>
+  <p>Area: ${data.area}</p>
+  <h5>TimeZones: ${data.timezones[0]}</h5>
+  <h2>Native Name: ${data.nativeName}</h2>
+  <p>Currencies Code: ${data.currencies[0].code}</p>
+  <p>Currencies Name: ${data.currencies[0].name}</p>
+  <p>Languages Name: ${data.languages[0].name}</p>
+  `;
+  container.appendChild(newDiv);
+  document.getElementById("closeBTN").addEventListener("click", () => {
+    const container = document.getElementById("modal-body");
+    container.childNodes[1].remove();
+  });
 };
