d62e546c597cb0e7f8907e083f7216f38855f309
user <ujjalroy7862@gmail.com>
2023-02-25 01:09:30 +0600

    done the filter functionality

--- AdvanceJS/countries.js
+++ AdvanceJS/countries.js
@@ -1,7 +1,36 @@
 const loadData = async () => {
-  const api = await fetch("https://restcountries.com/v2/all");
+  const api = await fetch(`https://restcountries.com/v2/all`);
   const jsonData = await api.json();
   displayCountries(jsonData);
+};
+
+const loadFilterRegionData = async (data) => {
+  const container = document.getElementById("country-info");
+  container.innerHTML = "";
+  const api = await fetch(`https://restcountries.com/v3.1/region/${data}`);
+  const jsonData = await api.json();
+  fillterCountryDisplay(jsonData);
+};
+
+const fillterCountryDisplay = (data) => {
+  const container = document.getElementById("country-info");
+  data.map((el) => {
+    // console.log(el.name.common);
+    const creeateDiv = document.createElement("div");
+    const { capital, population } = el;
+    // console.log(el.alpha2Code);
+    creeateDiv.innerHTML = `
+    <h2>Name: ${el.name.common}</h2>
+    <h3>Capital: ${capital}</h3>
+    <h4>Population: ${population}</h4>
+    <div id="card-photo">
+      <img src="${el.flags.png}" id="img" alt="">
+    </div>
+    <button onclick="countriesDetails('${el.alpha2Code}')" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Details</button>
+    
+    `;
+    container.appendChild(creeateDiv);
+  });
 };
 
 const displayCountries = (countries) => {
@@ -24,7 +53,6 @@ const displayCountries = (countries) => {
   });
 };
 
-loadData();
 const countriesDetails = async (code) => {
   const url = `https://restcountries.com/v2/alpha/${code}`;
 
@@ -53,3 +81,11 @@ const countryDetail = (data) => {
     container.childNodes[1].remove();
   });
 };
+
+document.getElementById("option").addEventListener("change", () => {
+  const selectValue = document.getElementById("option").value;
+  loadFilterRegionData(selectValue);
+  console.log(selectValue);
+});
+
+loadData();
