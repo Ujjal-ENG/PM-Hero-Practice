7411fdac2e0c682c248dc55beeec594b3ec49eb0
user <ujjalroy7862@gmail.com>
2023-02-27 00:06:41 +0600

    done the call and apply method

--- AdvanceJS/object.js
+++ AdvanceJS/object.js
@@ -1,17 +1,24 @@
 // create object using object literals
 
-// const player = {};
-// player.name = "Ujjal";
-// player.speciality = "HardWorker";
-// player.work = () => {
-//   console.log(`Now ${player.name} currently running web development`);
-// };
+const player = {};
+player.name = "Ujjal";
+player.speciality = "HardWorker";
+player.work = () => {
+  console.log(`Now ${player.name} currently running web development`);
+};
+
+const player2 = {};
+
+// const word = player.work.bind(player2);
+
+const work = player.work.call(player2);
+console.log(player2);
 
 // console.log(player);
 // player.work();
 
-const bottle = { color: "yellow", price: 50, isCleaned: true, capacity: 1 };
-const entries = Object.entries(bottle);
-entries.forEach((e) => {
-  console.log(`Key: ${e[0]} and value is : ${e[1]}`);
-});
+// const bottle = { color: "yellow", price: 50, isCleaned: true, capacity: 1 };
+// const entries = Object.entries(bottle);
+// Object.entries(bottle).forEach((e) => {
+//   console.log(`Key: ${e[0]} and value is : ${e[1]}`);
+// });
