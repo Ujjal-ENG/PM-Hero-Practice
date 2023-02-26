1588ed7800cd22156618fae1f2f82682b11bdc5d
user <ujjalroy7862@gmail.com>
2023-02-26 15:14:07 +0600

    done the showmore button also the maintain proper functionality

--- 100sJSDOMProject/PhoneHunter/index.js
+++ 100sJSDOMProject/PhoneHunter/index.js
@@ -1,58 +1,33 @@
 // document.getElementById("progressBar").classList.remove("invisible");
 
-const fetchData = async (query, length) => {
+const fetchData = async (query) => {
   try {
     const dataAPI = await fetch(
       `https://openapi.programming-hero.com/api/phones?search=${query}`
     );
     const data = await dataAPI.json();
     showData(data.data, length);
+    restData(data.data, length);
   } catch (error) {
     console.log(error);
   }
 };
 
-const showData = (data, length) => {
+const showData = (data) => {
   const container = document.getElementById("card-items");
-  console.log(length);
   if (data.length === 0) {
     container.innerHTML = "";
     document.getElementById("alertMessage").classList.remove("invisible");
     document.getElementById("showMoreBTN").classList.add("invisible");
   } else {
-    document.getElementById("progressBar").classList.add("invisible");
-    document.getElementById("alertMessage").classList.add("invisible");
-
-    container.innerHTML = "";
-
-    // display only 10 phones
-    data.slice(0, 10).forEach((el) => {
-      const createDiv = document.createElement("div");
-      const { brand, phone_name, image, slug } = el;
-      createDiv.innerHTML = `
-    <div class="card w-78 bg-base-100 shadow-xl ">
-    <figure><img src="${image}" alt="${phone_name}" /></figure>
-    <div class="card-body">
-      <h2 class="card-title">${brand + " " + phone_name}</h2>
-      <div class="card-actions justify-end">
-        <label for="my-modal-3" class="btn btn-primary "onclick="showDeatils('${slug}')">Details</label>
-      </div>
-    </div>
-  </div>
-    `;
-      container.appendChild(createDiv);
-    });
+    displayData(data, 10);
   }
-};
-const processSearch = (length) => {
-  const inputValue = document.getElementById("searchInput").value;
-  fetchData(inputValue, length);
 };
 
 const restData = (data) => {
   document.getElementById("showMoreBTN").addEventListener("click", () => {
-    processSearch(data.length);
-    console.log("hello");
+    // display only rest of the phones
+    displayData(data, 11);
   });
 };
 
@@ -107,8 +82,41 @@ const showModalData = (data) => {
 };
 
 const toggleFunction = () => {
-  processSearch(10);
+  const inputValue = document.getElementById("searchInput").value;
+  fetchData(inputValue, length);
   document.getElementById("searchInput").value = "";
   document.getElementById("progressBar").classList.remove("invisible");
   document.getElementById("showMoreBTN").classList.remove("invisible");
+};
+
+const displayData = (data, length) => {
+  const container = document.getElementById("card-items");
+  document.getElementById("progressBar").classList.add("invisible");
+  document.getElementById("alertMessage").classList.add("invisible");
+
+  container.innerHTML = "";
+  let phone;
+  if (length === 10) {
+    phone = data.slice(0, 10);
+  } else {
+    phone = data.slice(10, data.length);
+  }
+  console.log(phone);
+
+  phone.forEach((el) => {
+    const createDiv = document.createElement("div");
+    const { brand, phone_name, image, slug } = el;
+    createDiv.innerHTML = `
+  <div class="card w-78 bg-base-100 shadow-xl ">
+  <figure><img src="${image}" alt="${phone_name}" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">${brand + " " + phone_name}</h2>
+    <div class="card-actions justify-end">
+      <label for="my-modal-3" class="btn btn-primary "onclick="showDeatils('${slug}')">Details</label>
+    </div>
+  </div>
+</div>
+  `;
+    container.appendChild(createDiv);
+  });
 };
