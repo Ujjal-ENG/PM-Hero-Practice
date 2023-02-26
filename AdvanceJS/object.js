115cd4070afa79096ff73380c0df8be394f86454
user <ujjalroy7862@gmail.com>
2023-02-26 23:31:49 +0600

    done the entries

--- AdvanceJS/object.js
+++ AdvanceJS/object.js
@@ -1,11 +1,17 @@
 // create object using object literals
 
-const player = {};
-player.name = "Ujjal";
-player.speciality = "HardWorker";
-player.work = () => {
-  console.log(`Now ${player.name} currently running web development`);
-};
+// const player = {};
+// player.name = "Ujjal";
+// player.speciality = "HardWorker";
+// player.work = () => {
+//   console.log(`Now ${player.name} currently running web development`);
+// };
 
-console.log(player);
-player.work();
+// console.log(player);
+// player.work();
+
+const bottle = { color: "yellow", price: 50, isCleaned: true, capacity: 1 };
+const entries = Object.entries(bottle);
+entries.forEach((e) => {
+  console.log(`Key: ${e[0]} and value is : ${e[1]}`);
+});
