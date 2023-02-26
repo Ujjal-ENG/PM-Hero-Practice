1d51aabf8a1b624b4beac32db5f9a63e009d0523
user <ujjalroy7862@gmail.com>
2023-02-27 02:51:25 +0600

    done this project

--- 100sJSDOMProject/WeatherApp/index.js
+++ 100sJSDOMProject/WeatherApp/index.js
@@ -6,10 +6,36 @@ const options = {
   },
 };
 
-fetch(
-  "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Dhaka",
-  options
-)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
+const fetchCity = (city) => {
+  fetch(
+    `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${
+      city ? city : "Dhaka"
+    }`,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      // response.cloud_pct;
+      feels.innerHTML = response.feels_like;
+      humidity.innerHTML = response.humidity;
+      feelhumidity.innerHTML = response.humidity;
+      maxtemperaturs.innerHTML = response.max_temp;
+      mintemperaturs.innerHTML = response.min_temp;
+      sunrise.innerHTML = response.sunrise;
+      sunset.innerHTML = response.sunset;
+      temperaturs.innerHTML = response.temp;
+      temperaturs1.innerHTML = response.temp;
+      winddeg.innerHTML = response.wind_degrees;
+      windinfo.innerHTML = response.wind_speed;
+      windinfo1.innerHTML = response.wind_speed;
+      console.log(response);
+    })
+    .catch((err) => console.error(err));
+};
+
+document.getElementById("searchBTN").addEventListener("click", (e) => {
+  e.preventDefault();
+  const inputCity = document.getElementById("inputCity").value;
+  fetchCity(inputCity);
+  document.getElementById("city").innerHTML = inputCity;
+});
