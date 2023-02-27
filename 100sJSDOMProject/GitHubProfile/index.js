5d75238250b8d0221f794542960282bd91d3e5ae
user <ujjalroy7862@gmail.com>
2023-02-27 19:57:26 +0600

    fixed some design portion error

---
+++ 100sJSDOMProject/GitHubProfile/index.js
@@ -0,0 +1,6 @@
+// const fetchData = async () => {
+//   const fetchAPi = await fetch(`https://api.github.com/users?per_page=10`);
+//   const data = await fetchAPi.json();
+//   console.log(data);
+// };
+// fetchData();
