e7111ab4a36ae2ce93ec4ef54d622a24a9e9ae2a
user <ujjalroy7862@gmail.com>
2023-02-27 16:58:42 +0600

    add some error message based on the context

--- 100sJSDOMProject/ShortenLink/index.js
+++ 100sJSDOMProject/ShortenLink/index.js
@@ -1,8 +1,33 @@
 const fetchData = async (link) => {
-  const fetchAPI = await fetch(``)
-}
+  try {
+    const fetchAPI = await fetch(
+      `https://api.shrtco.de/v2/shorten?url=${link}`
+    );
+    const data = await fetchAPI.json();
+    showResult(data.result);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
+const showResult = (data) => {
+  errorHandle(data);
+  
+};
 
+const errorHandle = (data) => {
+  if (data === undefined) {
+    document.getElementById("error").removeAttribute("hidden");
+  } else {
+    document.getElementById("error").setAttribute("hidden", true);
+  }
+};
+
+document.getElementById("shortenBTN").addEventListener("click", (e) => {
+  e.preventDefault();
+  const inputLink = document.getElementById("inputLink").value;
+  fetchData(inputLink);
+});
 
 function myFunction() {
   // Get the text field
