236e64dd2ec7d0210cad5106a6d355a58b7976e1
user <ujjalroy7862@gmail.com>
2023-02-28 00:01:45 +0600

    check the number accoding to the question

--- AdvanceJS/closure_encap_private.js
+++ AdvanceJS/closure_encap_private.js
@@ -1,13 +1,35 @@
-function kitchen() {
-  let roast = 0;
-  return function () {
-    roast++;
-    return roast;
-  };
-}
+// function kitchen() {
+//   let roast = 0;
+//   return function () {
+//     roast++;
+//     return roast;
+//   };
+// }
 
-const firstServer = kitchen();
-console.log(firstServer());
-console.log(firstServer());
-console.log(firstServer());
-console.log(firstServer());
+// const firstServer = kitchen();
+// console.log(firstServer());
+// console.log(firstServer());
+// console.log(firstServer());
+// console.log(firstServer());
+
+// call back function
+
+// function greetingHandler() {
+//   console.log("Good Morning");
+// }
+
+// function greetSomeOne(greetingHandler) {
+//   greetingHandler();
+// }
+
+// greetSomeOne(greetingHandler);
+
+const checkNum = (num) => {
+  if (typeof num === "number") {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(checkNum(1));
