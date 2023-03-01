827b997ef45f05e27720735206c3da1b6f154f5d
user <ujjalroy7862@gmail.com>
2023-03-01 22:15:34 +0600

    done the alert messgage

--- 100sJSDOMProject/NewsPortal/index.js
+++ 100sJSDOMProject/NewsPortal/index.js
@@ -1,5 +1,6 @@
 const getID = (id) => document.getElementById(id);
 
+// for show the all Category
 const fetchCategoryAPI = async () => {
   const fetchCategoryAPIData = await fetch(
     "https://openapi.programming-hero.com/api/news/categories"
@@ -13,9 +14,25 @@ const showCategory = (data) => {
   data.forEach((el) => {
     categoryContainer.innerHTML += `
     <a href="#"
-    class=" rounded-lg p-2 hover:shadow-lg hover:p-2  focus:bg-violet-100 font-bold focus:text-violet-600 transition-all">${el.category_name}</a>
+    class=" rounded-lg p-2 hover:shadow-lg hover:p-2  focus:bg-violet-100 font-bold focus:text-violet-600 transition-all" onclick="fetchAllDataAPI('${el.category_id}')">${el.category_name}</a>
     `;
   });
 };
 
 fetchCategoryAPI();
+
+// fetch all data based on the category
+const fetchAllDataAPI = async (id) => {
+  const fetchCategoryAPIData = await fetch(
+    `https://openapi.programming-hero.com/api/news/category/${id}`
+  );
+  const categoryData = await fetchCategoryAPIData.json();
+  if (categoryData.data.length === 0) {
+    getID("red-alert").removeAttribute("hidden");
+    getID("success-alert").setAttribute("hidden", true);
+  } else {
+    getID("success-alert").removeAttribute("hidden");
+    getID("red-alert").setAttribute("hidden", true);
+  }
+  document.querySelector(".datafound").innerHTML = categoryData.data.length;
+};
