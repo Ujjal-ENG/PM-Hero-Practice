5ff8d4cf50169f10b9d42ebd1da26a33dd92a0fb
user <ujjalroy7862@gmail.com>
2023-03-02 00:18:02 +0600

    based on news show the data

--- 100sJSDOMProject/NewsPortal/index.js
+++ 100sJSDOMProject/NewsPortal/index.js
@@ -27,6 +27,7 @@ const fetchAllDataAPI = async (id, name) => {
     `https://openapi.programming-hero.com/api/news/category/${id}`
   );
   const categoryData = await fetchCategoryAPIData.json();
+  showTheCategoryData(categoryData.data);
   if (categoryData.data.length === 0) {
     getID("red-alert").removeAttribute("hidden");
     getID("success-alert").setAttribute("hidden", true);
@@ -36,4 +37,69 @@ const fetchAllDataAPI = async (id, name) => {
   }
   document.querySelector(".datafound").innerHTML = categoryData.data.length;
   document.querySelector(".categoryName").innerHTML = name;
+};
+
+const showTheCategoryData = (data) => {
+  const cardContainer = getID("card-container");
+  cardContainer.innerHTML = "";
+  data.forEach((el) => {
+    const containerCard = document.createElement("div");
+    containerCard.innerHTML = `
+    
+    <div class="card sm:card-side w-full bg-base-100 shadow-2xl mb-10">
+          <div class="w-full sm:w-1/3 h-[300px]">
+            <img src="${
+              el.thumbnail_url ? el.thumbnail_url : "./unsplash_EhTcC9sYXsw.png"
+            }" alt="Shoes" class="rounded-xl w-full h-full p-2" />
+          </div>
+
+          <div class="card-body">
+            <div class="cardInfo space-y-4">
+
+              <h2 class="card-title">${el.title}</h2>
+              <p>${el.details.slice(0, 400) + "...."}</p>
+            </div>
+
+            <div class="author-info grid grid-cols-4 pt-10">
+
+              <div class="flex items-center gap-4">
+                <div class="w-10">
+                  <img src="${
+                    el.author.img ? el.author.img : "./user-2.jpg"
+                  }" class="rounded-full" />
+                </div>
+
+                <div>
+                  <p class="font-bold">${el.author.name}</p>
+                  <p class="text-gray-500">${el.published_date}</p>
+
+                </div>
+              </div>
+
+              <div class="flex items-center justify-center gap-4">
+                <i class="fa-solid fa-eye"></i>
+                <p class="font-bold text-xl">${el.total_view}</p>
+              </div>
+
+              <div class="flex justify-center  items-center">
+                <div class="rating rating-sm">
+                  <input type="radio" name="rating-6" class="mask mask-star-2 bg-orange-400" />
+                  <input type="radio" name="rating-6" class="mask mask-star-2 bg-orange-400" checked />
+                  <input type="radio" name="rating-6" class="mask mask-star-2 bg-orange-400" />
+                  <input type="radio" name="rating-6" class="mask mask-star-2 bg-orange-400" />
+                  <input type="radio" name="rating-6" class="mask mask-star-2 bg-orange-400" />
+                </div>
+              </div>
+
+              <div class="flex items-center justify-end">
+                <i class="cursor-pointer fa-solid fa-arrow-right fa-2x text-violet-700" onclick="showModalDetails('${
+                  el._id
+                }')"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+    `;
+    cardContainer.appendChild(containerCard);
+  });
 };
