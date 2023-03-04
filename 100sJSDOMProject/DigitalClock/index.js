e4f0b2553ef620c3d62479686d30c1fa387fdf72
user <ujjalroy7862@gmail.com>
2023-03-05 02:24:23 +0600

    done the digital clock

--- 100sJSDOMProject/DigitalClock/index.js
+++ 100sJSDOMProject/DigitalClock/index.js
@@ -1,1 +1,14 @@
 const getID = (id) => document.getElementById(id);
+
+const id = setInterval(() => {
+  showTime();
+}, 1000);
+
+function showTime() {
+  const hours = new Date().getHours();
+  const minutes = new Date().getMinutes();
+  const seconds = new Date().getSeconds();
+  getID("hour").innerText = hours > 9 ? hours : "0" + hours;
+  getID("min").innerText = minutes > 9 ? minutes : "0" + minutes;
+  getID("sec").innerText = seconds > 9 ? seconds : "0" + seconds;
+}
