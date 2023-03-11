2c04f8f5e545332d0065ccd3eb3436ba88e0f8f3
user <ujjalroy7862@gmail.com>
2023-03-11 15:47:37 +0600

    i think now its okay,all the bugs now removed

--- issue-tracker/main.js
+++ issue-tracker/main.js
@@ -1,49 +1,77 @@
-document.getElementById('issueInputForm').addEventListener('submit', submitIssue);
+document
+  .getElementById("issueInputForm")
+  .addEventListener("submit", submitIssue);
 
 function submitIssue(e) {
-  const getInputValue = id => document.getElementById(id).value;
-  const description = getInputValue('issueDescription');
-  const severity = getInputValue('issueSeverity');
-  const assignedTo = getInputValue('issueAssignedTo');
-  const id = Math.floor(Math.random()*100000000) + '';
-  const status = 'Open';
+  const getInputValue = (id) => document.getElementById(id).value;
+  const description = getInputValue("issueDescription");
+  const severity = getInputValue("issueSeverity");
+  const assignedTo = getInputValue("issueAssignedTo");
+  const id = Math.floor(Math.random() * 100000000) + "";
+  const status = "Open";
 
   const issue = { id, description, severity, assignedTo, status };
   let issues = [];
-  if (localStorage.getItem('issues')){
-    issues = JSON.parse(localStorage.getItem('issues'));
+  if (localStorage.getItem("issues")) {
+    issues = JSON.parse(localStorage.getItem("issues"));
   }
   issues.push(issue);
-  localStorage.setItem('issues', JSON.stringify(issues));
+  localStorage.setItem("issues", JSON.stringify(issues));
 
-  document.getElementById('issueInputForm').reset();
+  document.getElementById("issueInputForm").reset();
   fetchIssues();
   e.preventDefault();
 }
 
-const closeIssue = id => {
-  const issues = JSON.parse(localStorage.getItem('issues'));
-  const currentIssue = issues.find(issue => issue.id === id);
-  currentIssue.status = 'Closed';
-  localStorage.setItem('issues', JSON.stringify(issues));
-  fetchIssues();
-}
+// const setStatusClosed = (id) => {
+//   let issues = JSON.parse(localStorage.getItem("issues"));
+//   let index = issues.findIndex((el) => el.id == id);
+//   issues[index].status = "closed";
+//   localStorage.setItem("issues", JSON.stringify(issues));
+//   fetchIssues();
+// };
 
-const deleteIssue = id => {
-  const issues = JSON.parse(localStorage.getItem('issues'));
-  const remainingIssues = issues.filter( issue.id !== id )
-  localStorage.setItem('issues', JSON.stringify(remainingIssues));
-}
+const setStatusClosed = (id) => {
+  const issues = JSON.parse(localStorage.getItem("issues"));
+  const currentIssue = issues.find((issue) => issue.id == id);
+  currentIssue.status = "Closed";
+  localStorage.setItem("issues", JSON.stringify(issues));
+  fetchIssues();
+};
+
+const deleteIssue = (id) => {
+  const issues = JSON.parse(localStorage.getItem("issues"));
+  const remainingIssues = issues.filter((el) => {
+    return el.id != id;
+  });
+  localStorage.setItem("issues", JSON.stringify(remainingIssues));
+  fetchIssues();
+};
+
+const deleteAge = (key, value) => {
+  let ages = JSON.parse(localStorage.getItem(key));
+
+  // find the index of the item you want to delete
+  let index = ages.findIndex(function (age) {
+    return age.age === value;
+  });
+
+  // remove the item at that index using splice()
+  ages.splice(index, 1);
+
+  // store the modified array back to local storage under the same key
+  localStorage.setItem(key, JSON.stringify(ages));
+};
 
 const fetchIssues = () => {
-  const issues = JSON.parse(localStorage.getItem('issues'));
-  const issuesList = document.getElementById('issuesList');
-  issuesList.innerHTML = '';
+  const issues = JSON.parse(localStorage.getItem("issues"));
+  const issuesList = document.getElementById("issuesList");
+  issuesList.innerHTML = "";
 
   for (var i = 0; i < issues.length; i++) {
-    const {id, description, severity, assignedTo, status} = issues[i];
+    const { id, description, severity, assignedTo, status } = issues[i];
 
-    issuesList.innerHTML +=   `<div class="well">
+    issuesList.innerHTML += `<div class="well">
                               <h6>Issue ID: ${id} </h6>
                               <p><span class="label label-info"> ${status} </span></p>
                               <h3> ${description} </h3>
@@ -53,4 +81,4 @@ const fetchIssues = () => {
                               <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
                               </div>`;
   }
-}
+};
